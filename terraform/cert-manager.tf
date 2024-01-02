fd3b7c893e6ec17cb070e27277bf3f5c2f0ae0cb
ElArcangelSax <ritualforce@gmail.com>
2024-01-02 01:12:07 -0300

    Corrected cert-manager.tf file

--- terraform/cert-manager.tf
+++ terraform/cert-manager.tf
@@ -23,3 +23,27 @@ resource "helm_release" "cert_manager" {
     kubernetes_namespace.cert_manager
   ]
 }
+
+locals {
+  clusterissuer = "clusterissuer.yaml"
+}
+
+data "kubectl_file_documents" "clusterissuer" {
+  content = file(local.clusterissuer)
+}
+
+resource "kubectl_manifest" "clusterissuer" {
+  for_each  = data.kubectl_file_documents.clusterissuer.manifests
+  yaml_body = each.value
+  depends_on = [
+    data.kubectl_file_documents.clusterissuer
+  ]
+}
+
+resource "null_resource" "clusterissuer_create" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      kubectl apply -f clusterissuer.yaml -n iacchallenge
+    EOT
+  }
+}
